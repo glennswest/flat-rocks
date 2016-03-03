d5edfd41ec3245b6afd831bf819f4679d639a80c
jmanero <john.manero@gmail.com>
2016-03-02 22:05:27 -0500

    Remove embedded dependencies. We're using dynamic linking. Hang on to your butts.

--- deps/leveldb/leveldb-rocksdb/util/build_version.cc
+++
@@ -1,6 +0,0 @@
-#include "build_version.h"
-
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:56589ab81f6827ff7402e31b24a6d548f29a524f";
-const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Thu Nov 21 15:45:16 EST 2013";
-const char* rocksdb_build_compile_date = __DATE__;
-const char* rocksdb_build_compile_time = __TIME__;
